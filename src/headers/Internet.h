c55e25d15e03b039044985ec98fe03a00194d2ab
Jagadish Shrestha <Shresthadev403@gmail.com>
2023-01-09 09:34:33 +0545

    webserver

--- src/headers/Internet.h
+++ src/headers/Internet.h
@@ -127,3 +127,20 @@ void sendPostRequest(){
       Serial.println("WiFi Disconnected");
     }
 }
+
+
+
+String getWebPage(){
+    String html ="<html lang=\"en\"> <head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>Document</title> </head> <body> hello its me yo </body> </html>";
+    return html;
+}
+
+
+/////////webpage section
+void handleServerOnConnect(){
+  Serial.println("on conncet ");
+}
+
+void handleServerOnNotFound(){
+Serial.println("server not found");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 
 // #include<ESP8266WiFi.h>
-
+#include <ESP8266WebServer.h>
 
 // my custom header files
 #include<headers/Sound.h>
 #include<headers/Internet.h>
+
+//server instance
+ESP8266WebServer server(80);
 
 
 const  unsigned  char active_buzzer= D5;
@@ -27,10 +30,26 @@ connectWifiRuntime();
 // sendGetRequest(" http://c305-202-51-69-31.ngrok.io/products ");
 
 
+
+// create a server on nodemcu for handle ing webpage
+
+ server.on(F("/"), []() {
+    server.send(200, "text/html",getWebPage() );
+  });
+  
+  server.begin();
+   Serial.println("HTTP server started");
+
+
+
 }
 
 void loop () {
-sendPostRequest();
+// sendPostRequest();
+
+
+//handle request on server of nodemcu
+server.handleClient();
  playBuzzer(active_buzzer,1,1000); 
  delay(10000);
 
